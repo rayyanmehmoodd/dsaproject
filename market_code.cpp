2626fe5ae69e9d06de4d40fdc9fa39184cb4ccb0
rayyanmehmoodd <rayyanmehmoodoff@gmail.com>
2024-12-20 23:58:15 +0500

    added product search

--- market_code.cpp
+++ market_code.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 using namespace std;
+
+int search(int);
+int display();
+string check(int); 
+
 struct node
  {
 	int ID;
@@ -53,4 +58,56 @@ struct node *head=NULL;
 	system("cls");
     cout<<"\n\n\t\t\t\tThis product is Inserted!\n\n\n";
 
+	}
+
+	int search(int id)                     //    for search item in list
+ {
+ 	int count=1;
+ 	struct node *p=head;
+ 	while(p!=NULL)
+ 	{
+ 		if(p->ID==id)
+ 			break;
+ 		else
+ 			count++;
+ 			p=p->next;
+	}
+ 	return count;
+ }
+ ///////////////////////////////////////////////////////////////////////
+ int customhash(int x, int mod)
+{
+	return x % mod;
+
+}
+
+	void delPro()
+	{
+		system("cls");
+		display();
+		int id;
+		struct node *cur=head;
+		struct node *pre=head;
+		cout<<"\n\nEnter ID to delete that product:\n\n";
+		cin>>id;
+		 if (head == NULL)
+    {
+    	system("cls");
+        cout<<"List is empty"<<endl;
+    }
+	int pos=0;
+	int count=display();               //   for load no of nodes
+	pos=search(id);                        //   for check weather desire node is exist or not
+	if(pos<=count){
+
+		while(cur->ID!=id){                //  for delete middle area products
+			pre=cur;
+			cur=cur->next;
+}
+		pre->next=cur->next;
+		system("cls");
+		cout<<"\n<<item is deleted>>\n";
+	}else{
+		cout<<"\n<<<Not found>>\n\n";
+	}
 	}
