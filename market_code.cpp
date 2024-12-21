cbc198b8a10aa81f5a9ef7890e9d1e9af10f83af
rayyanmehmoodd <rayyanmehmoodoff@gmail.com>
2024-12-21 19:37:05 +0500

    added buying system

--- market_code.cpp
+++ market_code.cpp
@@ -1,3 +1,7 @@
+#include<iostream>
+#include<string>
+#include <sstream>
+#include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
 
@@ -166,4 +170,133 @@ struct node *head=NULL;
 	}
 }
 
-//////////////////////////////////////////////////////////////////////////////////////
+int display()
+{
+		system("cls");
+		int c=0;             //   c for count products
+		struct node *p=head;
+		cout<<"Existing products are:\n";
+		cout<<"ID\t\tProduct Name\t\tPrice\t\tQuantity\n";
+		cout<<"=================================================================|\n";
+		while(p!=NULL)
+		{
+			cout<<p->ID<<"\t\t"<<p->proName<<"\t\t\t"<<p->prePrice<<"\t\t\t"<<check(p->quantity)<<"\n"; //    call   check func and pass quantity
+			p=p->next;
+			c=c+1;
+		}
+		cout<<"\nTotal products in our store is : "<<c<<"\n\n\n";
+		return c;
+	    }
+////////////////////////////////////////////////////////////////////////////////////////
+	    string check(int quant)
+
+	{              //        check function
+	    int a = quant;
+       stringstream ss;
+       ss << a;
+       string quantity = ss.str();
+
+	    	if(quant<=0)
+	    	return "out of stock!";
+	    	else
+	    	return quantity;
+		}
+
+///////////////////////////////////////////////////////////////////////
+	void buy()
+	{
+        system("cls");
+        display();
+		string products[20];
+		   //   for display sold items
+		int pay=0,no,c=0,price,id,i=1;
+
+        if(head==NULL)
+        {
+	    cout<<"\n<<<<There is no items to buy>>>>\n\n";
+        }
+	    else
+        {
+		cout<<"How many items you want to buy!\n";
+		cin>>no; 
+        int count=display();           //   for store no of nodes  in c
+		while (i<=no)
+        {
+			struct node *cur=head;
+            int quant,cho;   a:           //   quant   for quantity  and cho for choice
+            cout<<"Enter id of item that you want to buy: ";
+            int id,pos=0;
+            cin>>id;
+            if(id==-1){system("cls"); return;	}
+	        pos=search(id);
+	        if(pos<=count)
+        {
+	                        //     item is available in store
+		 while(cur->ID!=id)
+        {
+			cur=cur->next;
+		 }
+           cout<<"How many quantities you want:";
+	       cin>>quant;
+	       if(cur->quantity<quant)
+           {
+               cout<<"\n\n\t\t\t----The Quantity You Entered is not available---"<<endl;
+               cout<<"\n\t\t\t-----(Press -1 for Back to Main Menu)------"<<endl;
+               goto a;
+               
+           }
+           products[c]=cur->proName;   // this will conatin the items buy names in array; 
+		   c++;   
+		   
+	       pay=pay+(cur->prePrice*quant);         //     calculate Bill
+	       cur->quantity=cur->quantity-quant;           //    change quantity
+	       i++;
+
+	     }
+	    else
+        {    
+             
+		cout<<"\n<<<<<<<<<This item is not available in our store at this time>>>>\n\n";
+		
+	    }
+}  string customer;
+        cout<<"\n\t\t Enter Your Name :"; cin>>customer;
+        enqueue(customer);
+	    system("cls");
+	    
+        cout<<"\n\n\n\n\t\t\tYou have bought : ";
+        for(int i=0;i<no;i++)
+      {              //    show that item you have bought
+        cout<<products[i]<<" ";
+       }
+       	if(pay>=3000){
+        price=pay*(0.90);           //    with 10% discount
+	    cout<<"\n\nOriginal price : "<<pay;
+        cout<<"\n with 10% discount: "<<price<<"\nThank you for shopping !\n\n";
+	}
+         else{
+         price=pay;           //    with no discount
+         cout<<"\n Bill Amount: "<<price<<"\nThank you for shopping !\n\n";
+		}
+		  
+		{
+    	ofstream fout; 
+		string line; 
+		fout.open("recent_customer.txt"); 
+	
+		while (fout) { 
+			getline(cin, line); 
+			if (line == "-1") 
+			break; 
+			fout << line << endl; 
+		} 
+			fout.close(); 
+			ifstream fin; 
+	}
+		  
+		  
+		   
+		  }
+            }
+
+
